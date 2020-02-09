5237bac82612cff195168fb1c3ff0f151605fd35
Vedant pople <vedantpople4@gmail.com>
2020-02-10 02:19:57 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -4,6 +4,7 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Scaffold(
+      
       appBar : new AppBar(
         title: new Text("My Chat App"),
 ),
