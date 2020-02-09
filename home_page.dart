829536ed8e3e12e22c8c06cc90e65c63baa709d7
Vedant pople <vedantpople4@gmail.com>
2020-02-10 02:20:08 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_new/ChatScreen.dart';
 class HomePage extends StatelessWidget{
+  
   @override
   Widget build(BuildContext context){
     return new Scaffold(
